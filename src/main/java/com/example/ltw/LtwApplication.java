893a47f797919befa96fd8033e7fd6acf51ee2ca
kullhi.124 <huykullkaq@gmail.com>
2023-06-08 20:04:19 +0700

    ltw

---
+++ src/main/java/com/example/ltw/LtwApplication.java
@@ -0,0 +1,13 @@
+package com.example.ltw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LtwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LtwApplication.class, args);
+	}
+
+}
